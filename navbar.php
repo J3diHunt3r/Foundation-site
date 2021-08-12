ff1b2a0f8c370529acefd4edc7b40a557073beb7
Carl Haricombe <73272379+J3diHunt3r@users.noreply.github.com>
2021-08-12 14:01:20 +0800

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,23 @@
+
+
+<link rel="stylesheet" href="assets/css/navbar.css">
+<script src="foundation.js"></script>
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="index.html">Home</a>
+  <a href="aboutus.html">Product gallery</a>
+  <a href="aboutus.html">About Us</a>
+  <a href="contactform.html">Contact Form</a>
+  
+  
+</div>
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+</script>
